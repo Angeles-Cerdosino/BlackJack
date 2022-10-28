d613b506537158daea0fb60291e54ee5fe285671
Angeles Cerdosino <angelescerdosi@gmail.com>
2022-10-27 21:22:39 -0300

    adding exceptions

---
+++ src/main/java/backend/BlackJack/exceptions/InvalidFilterInformation.java
@@ -0,0 +1,7 @@
+package backend.BlackJack.exceptions;
+
+public class InvalidFilterInformation extends RuntimeException {
+    public InvalidFilterInformation(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/backend/BlackJack/exceptions/ValidationError.java
@@ -0,0 +1,15 @@
+package backend.BlackJack.exceptions;
+
+import lombok.Getter;
+
+@Getter
+public class ValidationError {
+    private final String field;
+    private final String message;
+
+    public ValidationError(String field, String message) {
+        super();
+        this.field = field;
+        this.message = message;
+    }
+}
